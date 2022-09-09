86f83670bcb0137cf69797f5800aaf4d4b111226
PeterALX <quaypeter25@gmail.com>
2022-09-09 10:51:06 -0700

    add 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+* main - Entry point
+*
+* Return: 0
+*/
+int main(void)
+{
+	int n = 49;
+
+	while (n < 58)
+	{
+		putchar(n);
+		n++;
+	}
+	putchar('\n');
+	return (0);
+}
+
