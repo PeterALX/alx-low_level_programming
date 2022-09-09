e628f2f41f253d7c7905c4555704355a3e98af60
PeterALX <quaypeter25@gmail.com>
2022-09-09 14:18:13 -0700

    add 101

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int i = 48;
+	int j = 48;
+	int k = 48;
+	while (i <= 57)
+	{
+		j = i + 1;
+		while (j <= 57)
+		{
+			k = j + 1;
+			while (k <= 57)
+			{
+				putchar(i);
+				putchar(j);
+				if (i != 56 || j != 57 || k != 57)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+				k++;
+
+			}
+			j++;
+		}
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
+
