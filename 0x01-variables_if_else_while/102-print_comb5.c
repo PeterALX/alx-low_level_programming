ca12b7b4b820c8378217d0404f6a0143f8123a59
PeterALX <quaypeter25@gmail.com>
2022-09-09 16:21:18 -0700

    update 102

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -20,14 +20,9 @@ int main(void)
 			putchar((j / 10) + '0');
 			putchar((j % 10) + '0');
 			if (i != 98 || j !=99)
-			{
 				putchar(',');
-				putchar(' ');
-			}
-			
-
+			putchar(' ');	
 		}
 	}
-
 	return (0);
 }
