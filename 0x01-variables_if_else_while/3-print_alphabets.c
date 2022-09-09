45f2ed3e085ec50d229f2d5dc99ba26501137565
PeterALX <quaypeter25@gmail.com>
2022-09-09 06:20:49 -0700

    add 3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+* main - Entry point
+*
+* Return: 0
+*/
+int main(void)
+{
+	char n = 'A';
+	while (n <= 'z')
+	{
+		if ( n <= 'Z' || n >= 'a')
+		{
+			putchar(n);
+		}
+	}
+	putchar('\n');
+	return (0);
+}
+
