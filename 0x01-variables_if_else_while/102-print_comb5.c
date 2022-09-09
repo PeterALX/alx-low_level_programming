6b13f7f44efc4621af2b18582c3a3081f5803d64
PeterALX <quaypeter25@gmail.com>
2022-09-09 15:56:07 -0700

    update 102

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,47 +1,17 @@
 #include <stdio.h>
 
 /**
- * main - Entry point
- *
- * Return: 0
- */
+* main - Entry point
+*
+* Return: 0
+*/
 int main(void)
 {
-	int i = 48;
-	int j = 48;
-	int a = 48;
-	int b = 48;
-
-	while (i <= 57)
+	for (int i = 0; i <=98; i++)
 	{
-		j = 48;
-		while (j <= 57)
-		{
-			a = i;
-			while (a <= 57)
-			{
-				b = j + 1;
-				while (b <= 57)
-				{
-					putchar(i);
-					putchar(j);
-					putchar(' ');
-					putchar(a);
-					putchar(b);
-					if (i != 57 || j != 56 || a != 57 || b != 57)
-					{
-						putchar(',');
-						putchar(' ');
-					}
-					b++;
-				}
-			a++;
-			}
-			j++;
-		}
-		i++;
+		putchar((i / 10) + '0');
+		putchar((i % 10) + '0');
 	}
-	putchar('\n');
 	return (0);
 }
 
