0493f589706e10c76b9e250b692b6b6cc5b74cfb
PeterALX <quaypeter25@gmail.com>
2022-09-09 16:05:42 -0700

    update 102

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,22 +1,27 @@
 #include <stdio.h>
 
 /**
-* main - Entry point
-*
-* Return: 0
-*/
+ * main - Entry point
+ *
+ * Return: 0
+ */
 int main(void)
 {
 	int i = 0;
 
 	for (i = 0; i <=98; i++)
 	{
-		putchar((i / 10) + '0');
-		putchar((i % 10) + '0');
-		putchar(' ');
+		for (j = i; j <=99; j++)
+		{
+			putchar((i / 10) + '0');
+			putchar((i % 10) + '0');
+			putchar(' ');
+			putchar((j / 10) + '0');
+			putchar((j % 10) + '0');
+			putchar(' ');
 
 
+		}
+		return (0);
 	}
-	return (0);
-}
 
