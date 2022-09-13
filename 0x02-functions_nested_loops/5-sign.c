b576b6336bba25a493593f426cd4b58ea88e8b10
PeterALX <quaypeter25@gmail.com>
2022-09-13 16:13:07 -0700

    add 5

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,17 @@
+#include "main.h" 
+
+/**
+* print_sign - print the sign of a number
+*
+* Return: 0 or 1
+*/
+int print_sign(int n)
+{
+	if (n > 0)
+		return 1;
+	else if ( n < 0)
+		return -1;
+
+	return (0);
+}
+
