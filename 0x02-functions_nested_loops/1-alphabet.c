e57999be1d0f02fabbe08c6fcd3ba6baff55a08c
PeterALX <quaypeter25@gmail.com>
2022-09-13 14:52:48 -0700

    add 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include <_putchar.h>
+
+/**
+* print_alphabet - print the lower case alphabet
+*
+* Return: 0
+*/
+void print_alphabet(void)
+{
+	int i = 97;
+	while ( i < 122){
+		_putchar(i);
+		i++;
+	}
+	return (0);
+}
+
