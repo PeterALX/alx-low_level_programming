2c893d527d68c0a85c554aecbea3266028a27fcb
PeterALX <quaypeter25@gmail.com>
2022-09-14 06:20:50 -0700

    update 6

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 
 /**
-* main - Entry point
-*
-* Return: 0
+* _abs - compute the absolute value of a number
+* @k: the number  whose absolute value os to be found
+* Return: the absolut value of k
 */
 int _abs(int k)
 {
