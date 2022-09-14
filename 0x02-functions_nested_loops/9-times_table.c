e9e22db898df51379d53fbfc5601585ead74304f
PeterALX <quaypeter25@gmail.com>
2022-09-14 16:00:19 -0700

    add 9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -14,7 +14,16 @@ void times_table(void)
 		for(j = 0; j <= 9; j++)
 		{
 			k = i * j;
+			
+			if(j == 0 || k == 0)
+			{
+				_putchar('0');
+				if (j ==9)
+					continue;
+				_putchar(',');
+				_putchar(' ');
 
+			}
 			if ((k / 10) == 0)
 			{
 				if (j != 0)
