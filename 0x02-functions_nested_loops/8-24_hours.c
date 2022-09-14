bfdb929d69b8194c8c0c1e42a882931fc972be8f
PeterALX <quaypeter25@gmail.com>
2022-09-14 15:29:51 -0700

    add 8

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+* jack_bauer - print every minute of the day
+*
+* Return: void
+*/
+void jack_bauer(void)
+{
+	int j = 0;
+	int k = 0;
+
+	while (j < 24)
+	{
+		while (k < 60)
+		{
+			putchar((j / 10) + '0');
+			putchar((j % 10) + '0');
+			putchar(':');
+			putchar((k / 10) + '0');
+			putchar((k % 10) + '0');
+			putchar('\n');
+
+		}
+	}
+}
+
