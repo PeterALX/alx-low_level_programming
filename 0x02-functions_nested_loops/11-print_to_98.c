ff30830c2b9f99bc38aa1a5fb8dea4ac7956fef2
PeterALX <quaypeter25@gmail.com>
2022-09-14 17:21:05 -0700

    add 11

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -12,6 +12,21 @@ void print_to_98(int n)
 
 		while (n <= 98)
 		{
+			if ( n < 0)
+			{
+				_putchar('-');
+				n = n * -1;
+				if (n/10 ==0)
+				{
+					_putchar(n + '0');
+				}
+				else
+				{
+					_putchar((n/10) + '0');
+					_putchar((n % 10) + '0');
+				}
+			}
+			_putchar(n + '0');
 			_putchar(n + '0');
 			n++;
 		}
