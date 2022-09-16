6c9871eaa36ea4752a3984094f3b947aee004f7f
PeterALX <quaypeter25@gmail.com>
2022-09-16 05:49:02 -0700

    update

--- 0x03-debugging/tests.c
+++ 0x03-debugging/tests.c
@@ -7,9 +7,9 @@
 */
 int main(void)
 {
-	int month = 2;
+	int month = 1;
 	int day = 29;
-	int year = 2000;
+	int year = 2001;
 
 	printf("Date: %02d/%02d/%04d\n",month,day,year);
 	day = convert_day(month, day);
