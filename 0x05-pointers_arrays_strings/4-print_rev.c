18e6936a8f29c710ab2356ca5412525497cfa872
PeterALX <quaypeter25@gmail.com>
2022-09-20 03:44:17 -0700

    add 4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+* print_rev - print a string in reverse
+*
+* Return: void
+*/
+void print_rev(char *S)
+{
+	int i;
+	int len = _strlen(s);
+	
+	for (i = len - 1; i >= 0; i++)
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n');
+}
