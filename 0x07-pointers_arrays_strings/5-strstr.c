44a142ab6d6637da61c087e36c32d3e93d1acbfc
PeterALX <quaypeter25@gmail.com>
2022-09-27 07:18:06 -0700

    add 7

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+* _strstr - finds the first occurence of a substring in a string
+* @haystack: the string to be searched
+* @needle: the substring to be found
+* Return: pointer to the substring found or NULL if substring not found
+*/
+char *_strstr(char *haystack, char *needle)
+{
+	while(*haystack)
+	{
+		
+	}
+	return ();
+}
+
---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+* print_chessboard - prints a chessboard
+* @a: a pointer
+* Return: void
+*/
+void print_chessboard(char (*a)[8])
+{
+	int i,j;
+
+	while (i < 8)
+	{
+		while (j < 8)
+		{
+			_putchar(a[i][j]);
+		}
+		_putchar('\n');
+	}
+}
+
