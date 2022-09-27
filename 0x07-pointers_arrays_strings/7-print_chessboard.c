84822f793bd4e1ed784585083d72db31b17f5747
PeterALX <quaypeter25@gmail.com>
2022-09-27 13:26:50 -0700

    add 7

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -8,11 +8,11 @@
 void print_chessboard(char (*a)[8])
 {
 	int i,j;
-	
+
 	i = 0;
 	while (i < 8)
 	{
-		j=0;
+		j = 0;
 		while (j < 8)
 		{
 			_putchar(a[i][j]);
@@ -22,4 +22,3 @@ void print_chessboard(char (*a)[8])
 		i++;
 	}
 }
-
