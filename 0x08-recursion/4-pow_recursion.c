5276837af296dcb25cec9ecddb1d144dd40c36bf
PeterALX <quaypeter25@gmail.com>
2022-09-29 04:11:50 -0700

    add 4

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -10,8 +10,8 @@ int _pow_recursion(int x, int y)
 {
 	if (y < 0)
 		return (-1);
-	if (y == 0)
-		return (1);
-	return (_pow_recursion(x, --y));
+	if (y == 1)
+		return (x * x);
+	return (x * _pow_recursion(x, --y));
 }
 
