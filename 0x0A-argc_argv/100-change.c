5ba4367b75bdd966bae5111ee668a1f72866020b
PeterALX <quaypeter25@gmail.com>
2022-09-30 14:33:16 -0700

    add 100

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,42 @@
+#include "main.h"
+
+/**
+* main - Entry point
+* @argc: number of arguments
+* @argv: array of arguments
+* Return: 0
+*/
+int main(int argc, char **argv)
+{
+	int coinCount = 0;
+	int i = 0;
+	int coinOptions[5] = {25, 10, 5, 2, 1};
+	int amount = atoi(argv[1]);
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	if (amount < 0)
+	{
+		printf("0\n");
+		return (0);
+	}
+
+	while (i < 5)
+	{
+		if (coinOptions[i] > amount)
+		{
+			i++;
+			continue;
+		}
+		coinCount += amount / coinOptions[i];
+		amount %= coinOptions[i];
+		i++;
+	}
+	printf("%d\n", coinCount);
+	return (0);
+}
+
