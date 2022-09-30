d69d8685f1d4fb1d4b988e584635b7d6c8580bfd
PeterALX <quaypeter25@gmail.com>
2022-09-30 05:44:52 -0700

    add 1

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -2,11 +2,13 @@
 
 /**
 * main - Entry point
+* @argc: number of arguments
+* @argv: array ofarguments
 * Return: 0
 */
 int main(int argc, char **argv)
 {
-	printf("%d",argc);
+	printf("%d\n",argc);
 	return (0);
 }
 
