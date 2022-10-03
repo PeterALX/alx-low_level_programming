cb4180653007e51b2ef4fe318be17892a16b1390
PeterALX <quaypeter25@gmail.com>
2022-10-03 01:15:00 -0700

    add 1

---
+++ 0x09-static_libraries/main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+* main - ent 
+* 
+* 
+* Return: 
+*/
+int main(void)
+{
+	_putchar('k');
+	_putchar('\n');
+	return (0);
+}
+
---
+++ 0x09-static_libraries/putchar.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+* _putchar - prints a char
+* @k: the char 
+* Return: 0 
+*/
+int _putchar(char k)
+{
+	printf("%c",k);
+	return (0);
+}
+
