d73d3809e3c2f521e20894f546b2c2413f8426cc
PeterALX <quaypeter25@gmail.com>
2022-10-04 15:50:13 -0700

    add 1

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+* strdup - allocate some memory and copy a string into it
+* @str: the string to be copied
+*
+* Return: a pointer to the memory
+*/
+char *_strdup(char *str)
+{
+	int size;
+	char *mem;
+	
+	if (!str)
+		return (NULL);
+
+	size = 0;
+	while (str[size])
+		size++;
+
+	mem = malloc(sizeof(char) * (size));
+	if (!mem)
+		return (NULL);
+	while (*str)
+	{
+		*mem = *str;
+		mem++;
+		str++;
+	}
+	return (mem);
+}
+
