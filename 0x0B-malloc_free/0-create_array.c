ad7aebe75bd3a2cbc288d87affe4611ad1978592
PeterALX <quaypeter25@gmail.com>
2022-10-04 15:37:35 -0700

    add 0

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -12,7 +12,7 @@ char *create_array(unsigned int size, char c)
 	char *arr;
 	unsigned int i = 0;
 
-	arr = malloc((1 + size) * sizeof(char));
+	arr = malloc(size * sizeof(char));
 	if (!arr)
 		return (NULL);
 	if (size == 0) return (NULL);
