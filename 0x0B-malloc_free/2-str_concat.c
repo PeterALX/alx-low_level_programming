cc6171d2c03287aad45aa58dea503877c1258bfe
PeterALX <quaypeter25@gmail.com>
2022-10-04 16:17:02 -0700

    add 2

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+* str_concat - concatenates two strings 
+* @s1: string 1
+* @s2: string 2
+* Return: pointer to the concatenated string
+*/
+char *str_concat(char *s1, char * s2)
+{
+	int size;
+	char *str;
+
+	size = 0;
+	if (s1)
+	{
+		while (*s1)
+			size++;
+	}
+
+	if (s2)
+	{
+		while (*s2)
+			size++;
+	}
+
+	str = malloc(sizeof(char) * (size));
+
+	if (!str)
+		return (NULL);
+
+	return (str);
+}
+
--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -3,6 +3,6 @@
 
 char *create_array(unsigned int, char);
 char *_strdup(char *str);
-char *str_concat(char, char);
+char *str_concat(char*, char*);
 int **alloc_grid(int, int);
 void free_grid(int **grid, int height);
