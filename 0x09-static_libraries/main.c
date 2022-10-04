ff76b9f789f739467029eb4b8ebc2b46a8ea1f93
PeterALX <quaypeter25@gmail.com>
2022-10-04 02:37:06 -0700

    add 0

--- 0x09-static_libraries/main.c
+++ 0x09-static_libraries/main.c
@@ -26,7 +26,12 @@ int main(void)
 	printf("strncat not implemented yet: %s\n", _strncat("strncat!", "foo", 42));
 	printf("strncpy not implemented yet: %s\n", _strncpy("strncpy!", "foo", 42));
 	printf("strcmp not implemented yet: %d\n", _strcmp("3","4"));
-
+	printf("memset(k, 'a', 4): %s\n", _memset(k,'a', 4));
+	printf("memcpy(k, \"Trantor Terminus\", 17): %s\n", _memcpy(k,"Trantor Terminus", 17));
+	printf("strchr(k, 'r'): %p\n", _strchr(k, 'r'));
+	printf("strspn(k,\"aTr\"): %u\n", _strspn(k, "aTr"));
+	printf("strpbrk(k,\"ooooo7r\"): %p\n", _strpbrk(k,"ooooo7r"));
+	printf("strstr(k,\"tor\"): %p\n", _strstr(k, "tor"));
 
 	return (0);
 }
