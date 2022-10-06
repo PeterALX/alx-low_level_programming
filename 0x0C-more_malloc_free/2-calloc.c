ad00f69f6446735dd249ecd625de62b2e072fdda
PeterALX <quaypeter25@gmail.com>
2022-10-06 08:17:21 -0700

    add

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+* _calloc - allocate memory using malloc
+* @nmemb: number of elements
+* @size: size of each element
+* Return: pointer to the memory
+*/
+void *calloc(unsigned int nmemb, unsigned int size)
+{
+	void *k;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+
+	k = malloc(size * nmemb); 
+	if (!k)
+		return (NULL);
+	return (k);
+}
