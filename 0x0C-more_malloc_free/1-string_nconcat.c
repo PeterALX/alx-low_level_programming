97c513915c4fa26049f46a189c605cfc6ddb8ac2
PeterALX <quaypeter25@gmail.com>
2022-10-06 07:47:12 -0700

    add

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -8,11 +8,11 @@
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	unsigned int size = 0, i, strcount;
-
+	unsigned int size = 0, i, strcount = 0;
 	char *str;
-		if (!s1)
-			s1 = "";
+
+	if (!s1)
+		s1 = "";
 	if (!s2)
 		s2 = "";
 	while (s1[size])
--- 0x0C-more_malloc_free/tests.c
+++ 0x0C-more_malloc_free/tests.c
@@ -8,9 +8,7 @@
  */
 int main(void)
 {
-	char *concat;
-	concat = string_nconcat("Best ", "School !!!", 6);
-	printf("%s\n", concat);
-	free(concat);
+	char *concat = string_nconcat("Best ", "School !!!", 6);
+	printf("%s\n",concat);
 	return (0);
 }
