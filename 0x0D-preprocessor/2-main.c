088178e5358654ff63c007fa9990c4729a53e088
PeterALX <quaypeter25@gmail.com>
2022-10-12 07:56:41 +0000

    add 2

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,4 +1,4 @@
-
+#include <stdio.h>
 /**
 * main - entry point, print name of file it was compiled from
 *
@@ -6,6 +6,6 @@
 */
 int main(void)
 {
-	printf(__BASE_FILE__);
+	printf(__BASE_FILE__"\n");
 	return (0);
 }
