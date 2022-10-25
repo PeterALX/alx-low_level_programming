9f2557e44d699df93ef6912a4c5e04b15def8f9f
PeterALX <quaypeter25@gmail.com>
2022-10-25 19:26:57 +0000

    add

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -9,15 +9,16 @@ size_t print_list(const list_t *h)
 {
 	int list_len = 0;
 	const list_t *ptr = h;
+
 	while (ptr)
 	{
 
 		if (ptr->len == 0)
-			printf("[%u] %s\n",h->len, "(nil)");
+			printf("[%u] %s\n", h->len, "(nil)");
 		else
-			printf("[%u] %s\n",h->len, h->str);
+			printf("[%u] %s\n", h->len, h->str);
 		ptr = ptr->next;
 		list_len++;
 	}
-	return list_len;
+	return (list_len);
 }
