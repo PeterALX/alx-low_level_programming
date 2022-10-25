530c0c18f7c00e8882ff723a8535e3d94de5929f
PeterALX <quaypeter25@gmail.com>
2022-10-25 21:38:51 +0000

    add

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -1,37 +1,39 @@
 #include "lists.h"
 
 /**
-* add_node_end - adds a node to the end of the linked list 
-* @head: the head node of the list
-* @str: the contents of the new node
-* Return: pointer to the new node
-*/
+ * add_node_end - adds a node to the end of the linked list 
+ * @head: the head node of the list
+ * @str: the contents of the new node
+ * Return: pointer to the new node
+ */
 list_t *add_node_end(list_t **head, const char *str)
 {
 	int str_len;
-	list_t *ptr = *head;
+	list_t *ptr; 
 	list_t *new_node;
-
-	while (ptr)
-	{
-
-		ptr = ptr->next;
-	}
 
 	new_node = malloc(sizeof(list_t));
 	if (!new_node)
 		return (NULL);
 
-	ptr->next = new_node;
-
-	str_len = 0;
-	while (str[str_len])
-		str_len++;
-
-	new_node->len = str_len;
-	new_node->str = strdup(str);
 	new_node->next = NULL;
 
-	return (new_node);
+	if (*head == NULL)
+		*head = new_node;
+	else
+	{
+		ptr = *head;
+		while (ptr->next)
+			ptr = ptr->next;
 
+		ptr->next = new_node;
+
+		str_len = 0;
+		while (str[str_len])
+			str_len++;
+
+		new_node->len = str_len;
+		new_node->str = strdup(str);
+	}
+	return (new_node);
 }
