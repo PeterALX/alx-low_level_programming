473e5eb783e2b6e5ab70b3ad74add3fb8d43d18a
PeterALX <quaypeter25@gmail.com>
2022-10-25 22:07:09 +0000

    add 4

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+* free_list - frees the linked list memory
+* @head: the head node of the list
+* Return: void
+*/
+void free_list(list_t *head)
+{
+	list_t *ptr;
+
+	ptr = head;
+	while (head)
+	{
+		ptr = head;
+		head = head->next;
+		free(temp->str);
+		free(temp);
+	}
+	free(head);
+}
