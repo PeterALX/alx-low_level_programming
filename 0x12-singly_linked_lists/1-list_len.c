9ece4f0b2eb843b6e39b9c16ce07c48d5cc17d8a
PeterALX <quaypeter25@gmail.com>
2022-10-25 19:49:13 +0000

    add

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+* print_list - prints elements of a list
+* @h: the head node of the list
+* Return: the number of nodes in the list
+*/
+size_t print_list(const list_t *h)
+{
+	int list_len = 0;
+	const list_t *ptr = h;
+
+	while (ptr)
+	{
+
+		ptr = ptr->next;
+		list_len++;
+	}
+	return (list_len);
+}
