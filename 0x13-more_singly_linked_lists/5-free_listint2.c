7ea62d50d0a0b57add1c1badf59463d2f4ed4f80
PeterALX <quaypeter25@gmail.com>
2022-10-26 21:22:18 +0000

    add 5

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - frees the linked list memory
+ * @head: the head node of the list
+ * Return: void
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *ptr;
+
+	ptr = *head;
+	while (*head)
+	{
+		ptr = *head;
+		head = *head->next;
+		free(ptr);
+	}
+	free(*head);
+	*head = NULL;
+}
