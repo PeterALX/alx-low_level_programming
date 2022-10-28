928941fe407dd7d1e319868c95a7154e7773688d
PeterALX <quaypeter25@gmail.com>
2022-10-28 09:34:33 +0000

    update

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -13,8 +13,6 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	unsigned int i;
 	listint_t *new_node;
 
-	if (idx == 0)
-		return (add_nodeint(head, n));
 
 	i = 0;
 	ptr = *head;
