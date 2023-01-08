55d6c71bff6b9cd394b3e2c6cf84f2a29022b12a
PeterALX <quaypeter25@gmail.com>
2023-01-08 15:06:35 +0000

    addddd

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -16,7 +16,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 	new_table = malloc(sizeof(hash_table_t));
 	if (!new_table)
 		return (NULL);
-	array = malloc(sizeof(hash_node_t*) * size);
+	array = malloc(sizeof(hash_node_t *) * size);
 	if (!array)
 	{
 		free(new_table);
