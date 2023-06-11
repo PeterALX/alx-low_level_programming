d08e98dd9c5c203d75f7cbcddf90e9e67f8de69e
PeterALX <quaypeter25@gmail.com>
2023-06-11 09:58:28 +0000

    coin changing done

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,49 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - finds the minimum coins to give a particular amount of change
+ * @argc: number of command line args
+ * @argv: array of command line args
+ * Return:always zero
+ */
+int main(int argc, char *argv[])
+{
+	int balance;
+	int coins_given;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	balance = atoi(argv[1]);
+	while (balance > 0)
+	{
+		if (balance / 25 > 0)
+		{
+			coins_given += balance / 25;
+			balance %= 25;
+		}
+		else if (balance / 10 > 0)
+		{
+			coins_given += balance / 10;
+			balance %= 10;
+		}
+		else if (balance / 5 > 0)
+		{
+			coins_given += balance / 5;
+			balance %= 5;
+		}
+		else if (balance / 2 > 0)
+		{
+			coins_given += balance / 2;
+			balance %= 2;
+		}
+		coins_given += balance;
+		balance = 0;
+		printf("%d\n", coins_given);
+	}
+	return (0);
+}
