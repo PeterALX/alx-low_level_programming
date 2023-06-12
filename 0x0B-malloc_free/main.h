a54075410c4a5e534e7b090424c8c585ea578156
PeterALX <quaypeter25@gmail.com>
2023-06-12 09:52:05 +0000

    done 0

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -2,6 +2,8 @@
 #define _MALLOC_
 
 #include <stdlib.h>
+#include <stdio.h>
 
 char *create_array(unsigned int size, char c);
+void _putchar(char c);
 #endif
---
+++ 0x0B-malloc_free/putchar.c
@@ -0,0 +1,6 @@
+#include "main.h"
+
+void _putchar(char c)
+{
+	printf("%c\n", c);
+}
