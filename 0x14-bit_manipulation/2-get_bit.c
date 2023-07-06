d540efe6b1479fb6d0f3235750504ab017b9f218
PeterALX <quaypeter25@gmail.com>
2023-07-06 13:21:41 +0000

    set done

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -38,7 +38,6 @@ int get_bit(unsigned long int n, unsigned int index)
 		width--;
 	}
 	return (val);
-
 }
 
 /**
--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,9 +1,11 @@
 #ifndef _BIT_MANIPULATION_
 #define _BIT_MANIPULATION_
+#include <stdio.h>
 
 int _putchar(char c);
 unsigned int binary_to_uint(const char *b);
 int _pow(int x, int y);
 void print_binary(unsigned long int n);
 int get_bit(unsigned long int n, unsigned int index);
+int set_bit(unsigned long int *n, unsigned int index);
 #endif /* !_BIT_MANIPULATION_ */
