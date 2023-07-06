b122ef35192f3540bad553604f8435ed73514c77
PeterALX <quaypeter25@gmail.com>
2023-07-06 12:24:55 +0000

    done some more fixing

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -40,3 +40,21 @@ int get_bit(unsigned long int n, unsigned int index)
 	return (val);
 
 }
+
+/**
+ * _pow - returns x raised to the power of y
+ * @x: x
+ * @y: y
+ * Return: x raised to the power of y
+ */
+int _pow(int x, int y)
+{
+	int result = 1;
+
+	while (y > 0)
+	{
+		result *= x;
+		y--;
+	}
+	return (result);
+}
