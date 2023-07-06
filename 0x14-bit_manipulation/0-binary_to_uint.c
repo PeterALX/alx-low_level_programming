57c950ac1f91c010fc5ac3b67611684e36ae7f02
PeterALX <quaypeter25@gmail.com>
2023-07-06 12:08:37 +0000

    fix

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -23,3 +23,21 @@ unsigned int binary_to_uint(const char *b)
 	}
 	return (result);
 }
+
+/**
+ * _pow - returns x raised to the power of y
+ * @x: x
+ * @y: y
+ * Return: x raised to the power of y
+ */
+int _pow(int x, int y)
+{
+	int result = 1;
+
+	while (y > 0)
+	{
+		result *= x;
+		y--;
+	}
+	return (result);
+}
