97cf72f4585ae5ac0f75018cea8bf79a70330c74
PeterALX <quaypeter25@gmail.com>
2023-08-18 19:56:58 +0000

    edit print dlist

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -13,6 +13,5 @@ size_t print_dlistint(const dlistint_t *h)
 		h = h->next;
 		length++;
 	}
-	printf("-> %lu elements\n", length);
 	return (length);
 }
--- 0x17-doubly_linked_lists/main.c
+++ 0x17-doubly_linked_lists/main.c
@@ -1,25 +1,26 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
 #include "lists.h"
 
+/**
+ * main - check the code
+ *
+ * Return: Always EXIT_SUCCESS.
+ */
 int main(void)
 {
-	/* list_node *head = malloc(sizeof(list_node)); */
-	dlistint_t *head = NULL;
-	int i;
+    dlistint_t *head;
 
-	i = 0;
-	while (i < 5)
-	{
-		add_dnodeint_end(&head, i);
-		i++;
-	}
-	add_dnodeint(&head, -1);
-	add_dnodeint_end(&head, -1);
-	insert_dnodeint_at_index(&head, 3, 69);
-	delete_dnodeint_at_index(&head, 7);
-	print_dlistint(head);
-	reverse_list_node(&head);
-	print_dlistint(head);
-	free_dlistint(head);
-	return (0);
+    head = NULL;
+    add_dnodeint(&head, 0);
+    add_dnodeint(&head, 1);
+    add_dnodeint(&head, 2);
+    add_dnodeint(&head, 3);
+    add_dnodeint(&head, 4);
+    add_dnodeint(&head, 98);
+    add_dnodeint(&head, 402);
+    add_dnodeint(&head, 1024);
+    print_dlistint(head);
+    return (EXIT_SUCCESS);
 }
-
