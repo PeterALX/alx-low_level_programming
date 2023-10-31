9ff8dbae36964c7d399a1ea4aeb2731e572828b4
PeterALX <quaypeter25@gmail.com>
2023-10-31 10:15:23 +0000

    fix jack bauer

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -10,13 +10,13 @@ void jack_bauer(void)
 	int hours = 0;
 	int minutes = 0;
 
-	while (hours < 24) {
-		while (minutes < 60) {
+	while (hours < 24)
+	{
+		while (minutes < 60)
+		{
 			_putchar(hours / 10 + '0');
 			_putchar(hours % 10 + '0');
-			_putchar(' ');
 			_putchar(':');
-			_putchar(' ');
 			_putchar(minutes / 10 + '0');
 			_putchar(minutes % 10 + '0');
 			_putchar('\n');
