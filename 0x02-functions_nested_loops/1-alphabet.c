7fff24dc848c00d5c9f25dd528960000f412552e
PeterALX <quaypeter25@gmail.com>
2023-10-31 09:18:29 +0000

    done 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints the alphabet followed by a new line
+ *
+ * Return: void
+ */
+void print_alphabet(void)
+{
+	int i;
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
