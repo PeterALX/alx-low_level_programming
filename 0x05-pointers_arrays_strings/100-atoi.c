db1972c0133774b3f161309f4dd84a480a0adf32
PeterALX <quaypeter25@gmail.com>
2023-11-06 14:18:26 +0000

    done atoi fr fr no cap

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -29,29 +29,3 @@ int _atoi(char *s)
 
 	return (negative == -1 ? -num : num);
 }
-
-	/*
-	int sign = 1;
-	unsigned int total = 0;
-	char null_flag = 0;
-
-	while (*s)
-	{
-		if (*s == '-')
-			sign *= -1;
-
-		if (*s >= '0' && *s <= '9')
-		{
-			null_flag = 1;
-			total = total * 10 + *s - '0';
-		}
-		else if (null_flag)
-			break;
-		s++;
-	}
-
-	if (sign < 0)
-		total = (-total);
-
-	return (total);
-	*/
