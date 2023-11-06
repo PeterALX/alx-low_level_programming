08dfc1a779497f30b1d5c695cc594caf7f66410e
PeterALX <quaypeter25@gmail.com>
2023-11-06 14:10:58 +0000

    more debugging

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -7,7 +7,32 @@
  */
 int _atoi(char *s)
 {
-	int num = 0;
+	int sign = 1;
+	unsigned int total = 0;
+	char null_flag = 0;
+
+	while (*s)
+	{
+		if (*s == '-')
+			sign *= -1;
+
+		if (*s >= '0' && *s <= '9')
+		{
+			null_flag = 1;
+			total = total * 10 + *s - '0';
+		}
+		else if (null_flag)
+			break;
+		s++;
+	}
+
+	if (sign < 0)
+		total = (-total);
+
+	return (total);
+}
+
+	/*int num = 0;
 	int found_num = 0;
 	int negative = 1;
 
@@ -24,12 +49,7 @@ int _atoi(char *s)
 		{
 			break;
 		}
-		/* else if (*s != '-' && *s != '+' && *s != ' ')
-		{
-			negative = 1;
-		}*/
 		s++;
 	}
 
-	return (negative == -1 ? -num : num);
-}
+	return (negative == -1 ? -num : num); */
