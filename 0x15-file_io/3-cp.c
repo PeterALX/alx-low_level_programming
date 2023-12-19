7e6679f21d5f7a796f7029b2dab6b77987878be2
PeterALX <quaypeter25@gmail.com>
2023-12-19 18:45:57 +0000

    wrote some man docs

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -3,6 +3,16 @@
 #include <stdlib.h>
 #include <sys/types.h>
 #include <unistd.h>
+
+void close_descriptors(int from, int to);
+
+/**
+ * main - entry point into my sweet program
+ * @ac: number of commandline args 
+ * @av: the commandline args
+ *
+ * Return: 0 on success, appropriate errno on fail.
+ */
 int main(int ac, char **av)
 {
 	int file_from, file_to;
@@ -24,7 +34,7 @@ int main(int ac, char **av)
 	if (file_to < 0)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
-		close(file_from); /* add check */
+		close_descriptors(file_from, 0);
 		exit(99);
 	}
 	do {
@@ -32,21 +42,38 @@ int main(int ac, char **av)
 		if (rcount < 0)
 		{
 			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
-			close(file_from); /* check these bad boys */
-			close(file_to);
+			close_descriptors(file_from, file_to);
 			exit(98);
 		}
-		wcount = write(file_to, buf, rcount );
+		wcount = write(file_to, buf, rcount);
 		if (wcount < 0)
 		{
 			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
-			close(file_from); /* check these bad boys */
-			close(file_to);
+			close_descriptors(file_from, file_to);
 			exit(99);
 		}
 	} while (rcount > 0);
-	close(file_from);
-	close(file_to);
+	close_descriptors(file_from, file_to);
 	return (0);
 }
 
+/**
+ * close_descriptors - closes two file descriptors passed to it
+ * @from: first descriptor
+ * @to: second descriptor
+ *
+ * Return: eternal blackness(void) forgive my silly jokes
+ *
+ * Description: pass 0 if no descriptor to close
+ */
+void close_descriptors(int from, int to)
+{
+	if (from > 0)
+	{
+		close(from);
+	}
+	if (to > 0)
+	{
+		close(to);
+	}
+}
