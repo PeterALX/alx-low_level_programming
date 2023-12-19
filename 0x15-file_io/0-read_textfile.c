9d13f07b823ea1445fbeb709462c1ff35c6f990c
PeterALX <quaypeter25@gmail.com>
2023-12-19 13:04:01 +0000

    fix 0 againnn

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,5 +1,6 @@
 #include "main.h"
 #include <sys/types.h>
+#include <unistd.h>
 
 /**
  * read_textfile - reads a text file to stdout
@@ -33,5 +34,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		free(buf);
 		return (0);
 	}
+	free(buf);
+	close(fd);
 	return (wcount);
 }
