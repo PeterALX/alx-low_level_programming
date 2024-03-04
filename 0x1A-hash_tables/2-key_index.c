8d9db46c0a2272cb859233439a4c8d3c11abda70
PeterALX <quaypeter25@gmail.com>
2024-03-04 10:00:17 +0000

    fix doc for 2

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -3,6 +3,7 @@
  * key_index - returns an index in hash table for key
  * @key: the key
  * @size: size of the hash table
+ * Return: index in hash table for key
  */
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
