bc6b6b852e69a54dceb23c4a6d1fe0b666be8913
PeterALX <quaypeter25@gmail.com>
2024-03-04 09:37:36 +0000

    edit hash table create

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -4,14 +4,16 @@ hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *table = malloc(sizeof(hash_table_t));
 
-	if (!table)
-		return (NULL);
-	table->size = size;
-	table->array = malloc(sizeof(hash_node_t *) * size);
-	if (!table->array)
+	if (table)
 	{
-		free(table);
-		return (NULL);
+		table->size = size;
+		table->array = malloc(sizeof(hash_node_t *) * size);
+		if (!table->array)
+		{
+			free(table);
+			return (NULL);
+		}
+		/* probably good idea to initialize table->array to NULL */
 	}
 
 	return (table);
