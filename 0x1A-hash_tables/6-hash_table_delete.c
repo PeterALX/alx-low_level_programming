16bcebfe32d32aca1925b6c49da5a9f9e3b7772d
PeterALX <quaypeter25@gmail.com>
2024-03-10 09:31:27 +0000

    fixin hidden issue with del

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -10,17 +10,21 @@ void hash_table_delete(hash_table_t *ht)
 {
 	unsigned long  int i;
 	hash_node_t *tmp;
+	hash_node_t *next;
 
 	if (ht == NULL || ht->array == NULL)
 		return;
 
 	for (i = 0; i < ht->size; i++)
 	{
-		for (tmp = ht->array[i]; tmp != NULL; tmp = tmp->next)
+		tmp = ht->array[i];
+		while (tmp)
 		{
+			next = tmp->next;
 			free(tmp->key);
 			free(tmp->value);
 			free(tmp);
+			tmp = next;
 		}
 	}
 	free(ht->array);
