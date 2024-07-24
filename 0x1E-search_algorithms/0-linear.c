2606d4b29e302f09cec0dee965706eda8d466ce6
Peter ALX <quaypeter25@gmail.com>
2024-07-24 10:09:40 +0000

    search algos done 0

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,23 @@
+#include "search_algos.h"
+
+/**
+ * linear_search - linear search an array
+ * @array: the array to search
+ * @size: The size of the array
+ * Return: the first index where the array is found, -1 if not found
+ */
+int linear_search(int *array, size_t size, int value)
+{
+	int i;
+
+	if (!array)
+		return (-1);
+	for (i = 0; i < size; i++)
+	{
+		if (array[i] == value)
+			return (i);
+		printf("Value checked array[%d] = [%d]\n", i, array[i]);
+	}
+
+	return (-1);
+}
